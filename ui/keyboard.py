46165036bf65e33c56b6bd9387a345bef9b3b609
Vladimir Zatuliveter <zatuliveter@gmail.com>
2025-03-10 01:57:13 +0600

    constants in keyboard class.

--- ui/keyboard.py
+++ ui/keyboard.py
@@ -3,6 +3,13 @@ import pygame
 
 
 class Keyboard:
+
+    KEY_SIZE = 40.0
+    KEY_SPACING = 6.0
+    FONT_SIZE = 24.0
+    LINE_KEYS_COUNT = 15
+    SIZE = 0.7  # 70% of the screen
+
     def __init__(self):
         self.__x = 0
         self.__y = 0
@@ -18,14 +25,14 @@ class Keyboard:
             [("Tab", 1.5), ("Q", 1), ("W", 1), ("E", 1), ("R", 1), ("T", 1), ("Y", 1),
              ("U", 1), ("I", 1), ("O", 1), ("P", 1), ("[", 1), ("]", 1), ("\\", 1.5)],
 
-            [("CapsLock", 1.8), ("A", 1), ("S", 1), ("D", 1), ("F", 1), ("G", 1),
-             ("H", 1), ("J", 1), ("K", 1), ("L", 1), (";", 1), ("'", 1), ("Enter", 2.35)],
+            [("CAPS", 1.8), ("A", 1), ("S", 1), ("D", 1), ("F", 1), ("G", 1),
+             ("H", 1), ("J", 1), ("K", 1), ("L", 1), (";", 1), ("'", 1), ("ENTER", 2.35)],
 
-            [("LShift", 2.30), ("Z", 1), ("X", 1), ("C", 1), ("V", 1), ("B", 1),
-             ("N", 1), ("M", 1), (",", 1), (".", 1), ("/", 1), ("RShift", 3.0)],
+            [("SHIFT", 2.30), ("Z", 1), ("X", 1), ("C", 1), ("V", 1), ("B", 1),
+             ("N", 1), ("M", 1), (",", 1), (".", 1), ("/", 1), ("Shift", 3.0)],
 
-            [("LCtrl", 1.25), ("Win", 1.25), ("LAlt", 1.25), ("Space", 7.15),
-             ("Alt", 1.25), ("Fn", 1.25), ("Menu", 1.25), ("RCtrl", 1.25)]
+            [("CTRL", 1.25), ("Win", 1.25), ("ALT", 1.25), ("Space", 7.15),
+             ("Alt", 1.25), ("Fn", 1.25), ("Menu", 1.25), ("Ctrl", 1.25)]
         ]
         # self.__set_scale(1.0)
 
@@ -42,16 +49,17 @@ class Keyboard:
         if self.__screen_height != screen_height or self.__screen_width != screen_width:
             self.__screen_height = screen_height
             self.__screen_width = screen_width
-            scale_w = (screen_width * 0.6 / 15.0) / 40.0
+            scale_w = (screen_width * Keyboard.SIZE / Keyboard.LINE_KEYS_COUNT) / \
+                (Keyboard.KEY_SIZE + Keyboard.KEY_SPACING)
             self.__set_scale(scale_w)
 
     def __set_scale(self, scale: float):
         self.__scale = scale
-        self.__x = self.__screen_width / 2 - 8.5 * 40 * self.__scale
-        self.__y = self.__screen_height / 2 + 1.0 * 40 * self.__scale
-        self.__key_size = 40 * scale
-        self.__spacing = 6 * scale
-        self.__font = pygame.font.Font(None, int(24 * scale))
+        self.__x = self.__screen_width / 2 - (Keyboard.LINE_KEYS_COUNT / 2.0 + 1) * Keyboard.KEY_SIZE * self.__scale
+        self.__y = self.__screen_height / 2.5 + Keyboard.KEY_SIZE * self.__scale
+        self.__key_size = Keyboard.KEY_SIZE * scale
+        self.__spacing = Keyboard.KEY_SPACING * scale
+        self.__font = pygame.font.Font(None, int(Keyboard.FONT_SIZE * scale))
         self.__create_keys()
 
     def highlight_key(self, key: str):
