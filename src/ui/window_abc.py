f5979e5369c70810b5ea2d26fbe602b2188f5cc0
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2025-03-30 12:08:32 +0600

    Full-Screen on F11 in typing_window added, #35

--- src/ui/window_abc.py
+++ src/ui/window_abc.py
@@ -21,7 +21,7 @@ class WindowABC(ABC):
         screen: Surface,
         min_screen_size: Point,
         max_screen_size: Point
-    ):
+    ) -> Surface:
         min_width, min_height = min_screen_size
 
         for event in events:
@@ -34,7 +34,7 @@ class WindowABC(ABC):
                 new_height = event.h if event.h > min_height else min_height
                 screen = pygame.display.set_mode((new_width, new_height), pygame.RESIZABLE)
 
-            if event.type == pygame.KEYDOWN and keys[pygame.K_F11]:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
                 if screen.get_size() != max_screen_size:
                     self.__prev_screen_size = screen.get_size()
                     new_screen_size = max_screen_size
@@ -47,3 +47,5 @@ class WindowABC(ABC):
                     new_screen_size,
                     pygame.FULLSCREEN if self.__is_fullscreen else pygame.RESIZABLE
                 )
+
+        return screen
