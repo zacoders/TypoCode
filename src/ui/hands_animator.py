405ff105c86070ee37db2bf0b2067c7053da6bb5
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2025-04-15 21:53:02 +0600

    scale added, #58

--- src/ui/hands_animator.py
+++ src/ui/hands_animator.py
@@ -1,5 +1,6 @@
 
 
+from typing import Tuple
 import pygame
 from pygame import Surface
 
@@ -8,13 +9,16 @@ from ui.fingers_enum import FingersEnum
 
 
 class HandsAnimator:
-    def __init__(self):
-        hands_image = pygame.image.load(
-            get_resource_path("src/_content/images/arms/arms.png"))
-        self.__hands_image = pygame.transform.scale(
-            hands_image, (hands_image.width // 1.1, hands_image.height // 1.1))
 
-        images = [
+    WIDTH_SCALE = 0.46
+    HEIGHT_SCALE = 0.54
+
+    def __init__(self):
+        self.__original_hands_image = pygame.image.load(
+            get_resource_path("src/_content/images/arms/arms.png"))
+        self.__hands_image = self.__original_hands_image
+
+        self.__finger_images = [
             "src/_content/images/arms/left_thumb.png",
             "src/_content/images/arms/right_thumb.png",
             "src/_content/images/arms/left_index_finger.png",
@@ -28,31 +32,26 @@ class HandsAnimator:
             "src/_content/images/arms/start_buttons_fingers.png"
         ]
 
-        fingers = {}
+        self.__fingers = {}
 
-        for image in images:
+        for image in self.__finger_images:
             finger = pygame.image.load(get_resource_path(image))
             finger_name = f"{image[image.rfind('/') + 1:-4]}"
 
-            finger_image = pygame.transform.scale(
-                finger,
-                (finger.width // 1.1, finger.height // 1.1)
-            )
-
-            fingers[finger_name] = finger_image
+            self.__fingers[finger_name] = finger
 
         self.__fingers_enums = [
-            (fingers["left_little_finger"], FingersEnum.LEFT_LITTLE),
-            (fingers["left_ring_finger"], FingersEnum.LEFT_RING),
-            (fingers["left_middle_finger"], FingersEnum.LEFT_MIDDLE),
-            (fingers["left_index_finger"], FingersEnum.LEFT_INDEX),
-            (fingers["left_thumb"], FingersEnum.LEFT_THUMB),
-            (fingers["right_thumb"], FingersEnum.RIGHT_THUMB),
-            (fingers["right_index_finger"], FingersEnum.RIGHT_INDEX),
-            (fingers["right_middle_finger"], FingersEnum.RIGHT_MIDDLE),
-            (fingers["right_ring_finger"], FingersEnum.RIGHT_RING),
-            (fingers["right_little_finger"], FingersEnum.RIGHT_LITTLE),
-            (fingers["start_buttons_fingers"], FingersEnum.START_BUTTONS)
+            (self.__fingers["left_little_finger"], FingersEnum.LEFT_LITTLE),
+            (self.__fingers["left_ring_finger"], FingersEnum.LEFT_RING),
+            (self.__fingers["left_middle_finger"], FingersEnum.LEFT_MIDDLE),
+            (self.__fingers["left_index_finger"], FingersEnum.LEFT_INDEX),
+            (self.__fingers["left_thumb"], FingersEnum.LEFT_THUMB),
+            (self.__fingers["right_thumb"], FingersEnum.RIGHT_THUMB),
+            (self.__fingers["right_index_finger"], FingersEnum.RIGHT_INDEX),
+            (self.__fingers["right_middle_finger"], FingersEnum.RIGHT_MIDDLE),
+            (self.__fingers["right_ring_finger"], FingersEnum.RIGHT_RING),
+            (self.__fingers["right_little_finger"], FingersEnum.RIGHT_LITTLE),
+            (self.__fingers["start_buttons_fingers"], FingersEnum.START_BUTTONS)
         ]
 
         self.__finger_enum = FingersEnum.LEFT_LITTLE
@@ -62,7 +61,13 @@ class HandsAnimator:
         self.__last_blink_time = pygame.time.get_ticks()
         self.__blink_delay = 500  # 0.5 секунды
 
+    def __get_scale(self, image_size: Tuple[int, int], screen_size: Tuple[int, int]):
+        scale_w = image_size[0] / screen_size[0] / self.WIDTH_SCALE
+        scale_h = image_size[1] / screen_size[1] / self.HEIGHT_SCALE
+        return scale_w, scale_h
+
     def update(self):
+
         now = pygame.time.get_ticks()
         if now - self.__last_blink_time >= self.__blink_delay:
             self.__last_blink_time = now
@@ -77,23 +82,34 @@ class HandsAnimator:
                 self.__is_visible = True
 
     def draw(self, screen: Surface):
-        screen.blit(
-            self.__hands_image,
+        scale_w, scale_h = self.__get_scale(
+            (self.__original_hands_image.get_size()),
+            (screen.get_size())
+        )
+
+        self.__hands_image = pygame.transform.scale(
+            self.__original_hands_image,
             (
-                screen.get_width() // 2 - self.__hands_image.width // 2,
-                screen.get_height() - self.__hands_image.height
+                int(self.__original_hands_image.get_width() / scale_w),
+                int(self.__original_hands_image.get_height() / scale_h)
             )
         )
 
+        hands_x = screen.get_width() // 2 - self.__hands_image.get_width() // 2
+        hands_y = screen.get_height() - self.__hands_image.get_height() + 75
+
+        screen.blit(self.__hands_image, (hands_x, hands_y))
+
         if self.__drew_fingers < len(self.__fingers_enums) and self.__is_visible:
             finger_image, self.__finger_enum = self.__fingers_enums[self.__drew_fingers]
-            screen.blit(
+            finger_image = pygame.transform.scale(
                 finger_image,
                 (
-                    screen.get_width() // 2 - self.__hands_image.width // 2,
-                    screen.get_height() - self.__hands_image.height
+                    int(finger_image.get_width() / scale_w),
+                    int(finger_image.get_height() / scale_h)
                 )
             )
+            screen.blit(finger_image, (hands_x, hands_y))
 
     def get_finger_enum(self):
         return self.__finger_enum
