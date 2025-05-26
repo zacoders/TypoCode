5315ad001fed9aa15043e630fa257b175c2e004a
Vladimir Zatuliveter <zatuliveter@gmail.com>
2025-05-26 19:10:05 +0600

    fix starting level.

--- src/services/mentor.py
+++ src/services/mentor.py
@@ -5,7 +5,7 @@ from services.line_stats import LineStats
 class Mentor:
 
     def __init__(self):
-        self.__typing_level = 5
+        self.__typing_level = 0
 
     @property
     def typing_level(self) -> int: return self.__typing_level
