6c2bfc9cf63ae099c4e57e98276102d44530f794
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2025-06-03 09:45:59 +0600

    JavaScript generator added

--- src/generators/__init__.py
+++ src/generators/__init__.py
@@ -6,3 +6,4 @@ from .sql import SqlGenerator
 from .t_sql import TransactSqlGenerator
 from .python import PythonGenerator
 from .typescript import TypeScriptGenerator
+from .java_script import JavaScriptGenerator
